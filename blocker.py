466edf6a26ea8c9d8366e901d67b03970be81775
NVombat <nvombatkere@gmail.com>
2021-06-05 14:24:54 +0530

    Websites added to blockList

--- blocker.py
+++ blocker.py
@@ -8,15 +8,15 @@ temp_hosts_path = "/home/nvombat/Desktop/Website-Blocker/hosts"
 original_hosts_path = "/etc/hosts"
 #When visiting the webistes that have been blocked redirect to localhost
 redirect_ip = "127.0.0.1"
-#Lists of domain names of websites to block
-website_block_list = ["www.facebook.com", "facebook.com", "www.twitter.com",
-"twitter.com", "www.instagram.com", "instagram.com", "www.youtube.com", "youtube.com"]
+#Lists of domain names of websites to block 
+website_block_list = ["www.facebook.com", "facebook.com", "www.hotstar.com", "hotstar.com", "www.instagram.com", 
+"instagram.com", "www.youtube.com", "youtube.com", "www.web.whatsapp.com", "web.whatsapp.com"]
 
 
 #Runs indefinitely
 while True:
     #Set a time period to block the websites -> Current set to block from 8:00 am to 4:00 pm
-    if dt(dt.now().year, dt.now().month, dt.now().day,8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,16):
+    if dt(dt.now().year, dt.now().month, dt.now().day,15) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,16):
         print("Working Hours")
         #Open the hosts file in read-write mode
         with open(original_hosts_path, "r+") as f:
@@ -51,4 +51,4 @@ while True:
             #Remove all lines below it thus resulting in the original hosts file with no websites to be blocked
             f.truncate()
     #Keep checking conditions every n seconds
-    time.sleep(5)
+    time.sleep(10)
