479c37efa54b66ff3699c9040a2e6aa98e2c9a30
NVombat <nvombatkere@gmail.com>
2021-06-09 02:20:51 +0530

    final update

--- blocker.py
+++ blocker.py
@@ -1,6 +1,6 @@
-#Import libraries
 from datetime import datetime as dt
 import time
+
 
 #TEMP HOST FILE COPIED INTO FOLDER
 temp_hosts_path = "/home/nvombat/Desktop/Website-Blocker/hosts"
@@ -13,21 +13,17 @@ website_block_list = ["www.facebook.com", "facebook.com", "www.hotstar.com", "ho
 "instagram.com", "www.youtube.com", "youtube.com", "www.web.whatsapp.com", "web.whatsapp.com"]
 
 
-#Runs indefinitely
 while True:
     #Set a time period to block the websites -> Current set to block from 8:00 am to 4:00 pm
     if dt(dt.now().year, dt.now().month, dt.now().day,15) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,16):
         print("Working Hours")
-        #Open the hosts file in read-write mode
         with open(original_hosts_path, "r+") as f:
-            #Read contents of file
             cont = f.read()
             #print(cont)
             #Check if each website is in the hosts file
             for ws in website_block_list:
                 #If website is in file during block time
                 if ws in cont:
-                    #Skip
                     pass
                 #If website is not in file during block time
                 else:
@@ -36,13 +32,10 @@ while True:
     #If the time is outside that of the websites being blocked
     else:
         print("Free Hours")
-        #Open the hosts file in read-write mode
         with open(original_hosts_path, "r+") as f:
-            #Read contents line by line and store lines as items in a list
             cont = f.readlines()
             #Place the file pointer at the start of the file
             f.seek(0)
-            #Run through each line
             for line in cont:
                 #If the website to be blocked is not in the current line
                 if not any(ws in line for ws in website_block_list):
